8b5bcf2ea27d0c8b2a4a8757b7ef2f262d61d1dd
Anastasiya Mitrofanova <mitrofanovaa70@gmail.com>
2024-05-17 22:27:10 +0300

    закрытие попапа при клике снаружи

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setActiveSort } from '../redux/slices/filterSlice';
 
@@ -11,16 +11,27 @@ export const list = [
 function Sort() {
   const dispatch = useDispatch();
   const activeSort = useSelector((state) => state.filterSlice.activeSort);
-
   const [isOpen, setIsOpen] = useState(false);
+  const sortRef = useRef();
 
   const onChangeSort = (obj) => {
     dispatch(setActiveSort(obj));
     setIsOpen(false);
   };
 
+  useEffect(() => {
+    const onClickOutside = (event) => {
+      if (!event.composedPath().includes(sortRef.current)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.body.addEventListener('click', onClickOutside);
+    return () => document.body.removeEventListener('click', onClickOutside);
+  }, []);
+
   return (
-    <div className="sort">
+    <div className="sort" ref={sortRef}>
       <div className="sort__label">
         <b>Сортировка по</b>
         <span onClick={() => setIsOpen(!isOpen)}>{activeSort.name}</span>
